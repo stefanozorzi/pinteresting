2404a814c4001d79252fb912eb728fef38d3a1ee
stefanozorzi <sfnzrz@gmail.com>
2015-09-13 15:56:16 +0200

    Implemented Devise for Users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for heroku
+  # add real hosting 
+  config.action_mailer.default_url_options = { host: 'stefano-pinteresting.herokuapp.com/ ' }
 end
