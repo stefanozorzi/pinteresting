c2f088a0b73cb9cb394a81533be790f9edf031ed
stefanozorzi <sfnzrz@gmail.com>
2015-09-20 10:06:44 +0200

    changed config options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
     :storage => :s3,
       :region => ENV['AWS_REGION'],
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
+      :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
