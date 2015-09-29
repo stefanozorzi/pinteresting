fa44e7b68688ef9660c93ea04d715f2bd8f216f2
stefanozorzi <sfnzrz@gmail.com>
2015-09-29 08:36:20 +0200

    Revert "added paperclib.rb"
    This reverts commit 76d87c9effabdd2e45fdd26a09b7fca5dc8e207e.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,1 @@
-Paperclip::Attachment.default_options[:url] = ':stefanopinteresting.s3.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-1.amazonaws.com'
